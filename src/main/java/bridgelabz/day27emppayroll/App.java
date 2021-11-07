c6e61033fa2a47d1abfa86e248e914de9d0790bf
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-07 19:03:57 +0530

    Added Employee Payroll

---
+++ src/main/java/bridgelabz/day27emppayroll/App.java
@@ -0,0 +1,13 @@
+package bridgelabz.day27emppayroll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/bridgelabz/day27emppayroll/AppTest.java
@@ -0,0 +1,20 @@
+package bridgelabz.day27emppayroll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
